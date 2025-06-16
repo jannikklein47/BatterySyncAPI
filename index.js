3f58b8f2bbc1e03c68f931d28d47da4fd087d033
jannikklein47 <jannikklein0302@outlook.de>
2025-06-16 15:43:19 +0200

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require('dotenv').config()
 
+const cors = require('cors')
+
 const express = require("express");
 const routes = require("./routes");
 const db = require("./models")
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors()) 
+let corsOptions = { origin: '*', optionSuccessStatus: 200, 
+    methods: ['GET', 'PUT', 'POST', 'DELETE']}
+    
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
