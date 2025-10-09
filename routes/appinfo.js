c9c2fed63f0caec96d54281ae1b994a325e43ee4
jannikklein47 <jannikklein0302@outlook.de>
2025-10-09 10:37:34 +0200

    appinfo test 10

--- routes/appinfo.js
+++ routes/appinfo.js
@@ -30,15 +30,15 @@ router.post("/", async (req, res) => {
         const access = req.headers.adminCode
         if (access === process.env.adminCode) {
 
-            const recent = await AppInfos.findOne({attributes: { exclude: ['createdAt', 'updatedAt']},order: [['id', 'DESC']]})
+            let recent = await AppInfos.findOne({attributes: { exclude: ['createdAt', 'updatedAt']},order: [['id', 'DESC']]})
             recent = recent.dataValues
             delete recent.id
             
             console.log("Recent:", recent)
             
-            console.log("Create:", {...req.body, ...recent.dataValues})
+            console.log("Create:", {...recent.dataValues, ...req.body})
 
-            await AppInfos.create({...req.body, ...recent.dataValues})
+            await AppInfos.create({...recent.dataValues, ...req.body})
             res.status(200).send("Ok")
 
         } else res.status(403).send("Access denied")
