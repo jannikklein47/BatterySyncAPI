2a24c3bf4c61dcc71198f26665fd1892175248a8
jannikklein47 <jannikklein0302@outlook.de>
2025-10-09 10:31:48 +0200

    appinfo test 6

--- routes/appinfo.js
+++ routes/appinfo.js
@@ -14,8 +14,8 @@ router.get("/", async (req, res) => {
         const auth = req.headers.authorization
         const user = await Users.findOne({where: {password: auth}})
         if (user) {
-            console.log(await AppInfos.findAll({raw: true, order: [['id', 'DESC']]}))
-            const currentInfo = AppInfos.findAll({order: [['id', 'DESC']]})[0]
+            console.log(await AppInfos.findOne({order: [['id', 'DESC']]}))
+            const currentInfo = await AppInfos.findAll({order: [['id', 'DESC']]})[0]
             res.send(currentInfo)
         } else {
             res.status(403).send("Invalid access token")
