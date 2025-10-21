c61eeae43933f9f3dd7e741a712faa6429257131
jannikklein47 <jannikklein0302@outlook.de>
2025-10-22 01:37:04 +0200

    try out https

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const express = require("express");
 const routes = require("./routes");
 const db = require("./models")
+const fs = require('fs')
+
+const https = require('https')
 
 const app = express();
 const PORT = 3000;
@@ -19,9 +22,17 @@ app.use(express.json())
 
 app.use(routes);
 
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "localhost-key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "localhost.pem")),
+};
+
+const server = https.createServer(options, app);
+
+
 db.sequelize.sync().then(() => {
     console.log('✅ Datenbank synchronisiert');
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`🚀 Server läuft auf Port ${PORT}`);
     });
   }).catch((err) => {
