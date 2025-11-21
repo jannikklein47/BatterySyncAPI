df54717877cb5a5aba1bd8f83a168bbedf9571d9
jannikklein47 <jannikklein0302@outlook.de>
2025-11-21 14:47:05 +0100

    test: metrics route

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const login = require("./login.js")
-const battery = require("./battery.js")
-const device = require("./device.js")
-const notification = require("./notification.js")
-const appinfo = require('./appinfo.js')
+const login = require("./login.js");
+const battery = require("./battery.js");
+const device = require("./device.js");
+const notification = require("./notification.js");
+const appinfo = require("./appinfo.js");
+const metrics = require("./metrics.js");
 
 router.use("/battery", battery);
-router.use("/login", login)
-router.use("/device", device)
-router.use("/notification", notification)
-router.use('/appinfo', appinfo)
+router.use("/login", login);
+router.use("/device", device);
+router.use("/notification", notification);
+router.use("/appinfo", appinfo);
+router.use("/metrics", metrics);
 
 module.exports = router;
---
+++ routes/metrics.js
@@ -0,0 +1,92 @@
+const express = require("express");
+const models = require("../models");
+const { QueryTypes } = require("sequelize");
+
+const logs = models.logs;
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const requestCounts = await logs.query(
+      `
+        SELECT
+          DATE_TRUNC('minute', "createdAt") - 
+          MOD(EXTRACT(MINUTE FROM "createdAt")::int, 30) * INTERVAL '1 minute'
+            AS interval_start,
+          method,
+          COUNT(*) AS count
+        FROM logs
+        GROUP BY interval_start, method
+        ORDER BY interval_start ASC;
+        `,
+      { type: QueryTypes.SELECT }
+    );
+
+    const responseSizes = await logs.query(
+      `
+        SELECT
+          DATE_TRUNC('minute', "createdAt") - 
+            MOD(EXTRACT(MINUTE FROM "createdAt")::int, 30) * INTERVAL '1 minute'
+              AS interval_start,
+          SUM("resSize") AS total_res_size
+        FROM logs
+        GROUP BY interval_start
+        ORDER BY interval_start ASC;
+      `,
+      { type: QueryTypes.SELECT }
+    );
+
+    const errorCounts = await sequelize.query(
+      `
+      SELECT
+        DATE_TRUNC('minute', "createdAt") - 
+          MOD(EXTRACT(MINUTE FROM "createdAt")::int, 30) * INTERVAL '1 minute'
+            AS interval_start,
+        COUNT(*) AS error_count
+      FROM logs
+      WHERE error IS NOT NULL
+      GROUP BY interval_start
+      ORDER BY interval_start ASC;`,
+      { type: QueryTypes.SELECT }
+    );
+
+    const blockedCounts = await sequelize.query(
+      `
+      SELECT
+        DATE_TRUNC('minute', "createdAt") - 
+          MOD(EXTRACT(MINUTE FROM "createdAt")::int, 30) * INTERVAL '1 minute'
+            AS interval_start,
+        COUNT(*) AS text_present_count
+      FROM logs
+      WHERE text IS NOT NULL
+      GROUP BY interval_start
+      ORDER BY interval_start ASC;
+    `,
+      { type: QueryTypes.SELECT }
+    );
+
+    const successCounts = await sequelize.query(
+      `
+      SELECT
+        DATE_TRUNC('minute', "createdAt") - 
+          MOD(EXTRACT(MINUTE FROM "createdAt")::int, 30) * INTERVAL '1 minute'
+            AS interval_start,
+        COUNT(*) AS text_missing_count
+      FROM logs
+      WHERE text IS NULL
+      GROUP BY interval_start
+      ORDER BY interval_start ASC;
+    `,
+      { type: QueryTypes.SELECT }
+    );
+
+    console.log(
+      requestCounts,
+      responseSizes,
+      errorCounts,
+      blockedCounts,
+      successCounts
+    );
+  } catch (error) {}
+});
