dcd01c53ff59e02dc18d4372c6528953df74e3c3
jannikklein47 <jannikklein0302@outlook.de>
2025-11-21 19:24:13 +0100

    another fix

--- index.js
+++ index.js
@@ -25,17 +25,14 @@ app.use(cors(corsOptions));
 
 const limiter = rateLimit({
   windowMs: 1000, // 20 seconds
-  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-  // store: ... , // Redis, Memcached, etc. See below.
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 });
 
 app.use(limiter);
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   req.rawBodySize = new Blob([req.query + req.body]).size;
+  next();
 });
 
 app.use(routes);
