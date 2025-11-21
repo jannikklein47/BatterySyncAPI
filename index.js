46ea6899883d9357766869a262cf5f4b6fae1809
jannikklein47 <jannikklein0302@outlook.de>
2025-11-21 19:18:56 +0100

    changed: use express rate limit for ddos protection

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const fs = require("fs");
 const path = require("path");
 const https = require("https");
 
-const umbress = require("umbress");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 const PORT = 3000;
@@ -23,15 +23,16 @@ let corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.urlencoded({ extended: true }));
+const limiter = rateLimit.rateLimit({
+  windowMs: 1000, // 20 seconds
+  limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+  // store: ... , // Redis, Memcached, etc. See below.
+});
 
-app.use(
-  umbress({
-    rateLimiter: {
-      enabled: true,
-    },
-  })
-);
+app.use(limiter);
 
 app.use((req, res) => {
   req.rawBodySize = new Blob([req.query + req.body]).size;
