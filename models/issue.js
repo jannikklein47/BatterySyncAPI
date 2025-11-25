a3732a2c937f9cb296ea16a97a7560b36e9b5fb7
jannikklein47 <jannikklein0302@outlook.de>
2025-11-25 21:18:29 +0100

    fixed: issue user association

--- models/issue.js
+++ models/issue.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       issue.belongsTo(models.User, {
         foreignKey: "userId",
-        as: "issue",
+        as: "user",
       });
     }
   }
