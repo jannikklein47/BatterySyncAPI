8cf8e69421fdb474b2124760c9e5ddbe3de1e35e
jannikklein47 <jannikklein0302@outlook.de>
2025-11-27 08:30:52 +0100

    test

--- routes/issue.js
+++ routes/issue.js
@@ -160,8 +160,9 @@ router.post("/", async (req, res) => {
         created.userId
       );
 
-      if (data.notify && data.priority === 2) {
+      if (data.notify && (data.priority === 2 || data.priority === "2")) {
         const admin = await Users.findOne({ where: { admin: true } });
+        console.log("Sent notification to admin ", admin.id);
         sendUpdateNotification(
           "Kritisches Problem",
           "'" + created.title + "' erfordert deine Aufmerksamkeit.",
