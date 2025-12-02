f9eaa5d0e8a102eae5ab76b9f8c8f03c301821b8
jannikklein47 <jannikklein0302@outlook.de>
2025-12-02 15:18:11 +0100

    added: initial cleanup

--- jobs/notificationCleanup.js
+++ jobs/notificationCleanup.js
@@ -5,27 +5,32 @@ const { Op } = require("sequelize");
 
 const log = require("../services/logsystem");
 
+async function cleanup() {
+  try {
+    const twelveHoursAgo = new Date(Date.now() - 12 * 60 * 60 * 1000);
+
+    const deleted = await models.OrderedNotifications.destroy({
+      where: {
+        type: "CONTENT",
+        createdAt: {
+          [Op.lt]: twelveHoursAgo,
+        },
+      },
+    });
+
+    log("CRON: " + deleted + " notifications cleaned up.");
+  } catch (error) {
+    log("CRON failure", null, null, null, null, null, error);
+  }
+}
+
 /**
  * Schedules an hourly cronjob that cleans up old unreceived notifications
  */
 module.exports = function () {
+  cleanup();
   cron.schedule("0 * * * *", async () => {
-    try {
-      const twelveHoursAgo = new Date(Date.now() - 12 * 60 * 60 * 1000);
-
-      const deleted = await models.OrderedNotifications.destroy({
-        where: {
-          type: "CONTENT",
-          createdAt: {
-            [Op.lt]: twelveHoursAgo,
-          },
-        },
-      });
-
-      log("CRON: " + deleted + " notifications cleaned up.");
-    } catch (error) {
-      log("CRON failure", null, null, null, null, null, error);
-    }
+    await cleanup();
   });
   log("CRON 0 * * * * scheduled.");
 };
