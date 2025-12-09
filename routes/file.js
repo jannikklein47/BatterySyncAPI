bf77759fe9011c6645f46602c5db0aacbec476ab
jannikklein47 <jannikklein0302@outlook.de>
2025-12-09 19:57:01 +0100

    fixed: users can download apps without login

--- routes/file.js
+++ routes/file.js
@@ -12,20 +12,27 @@ const router = express.Router();
 
 router.get("/android", async (req, res) => {
   try {
-    const auth = req.headers.authorization;
-    const user = await Users.findOne({ where: { password: auth } });
-    if (user) {
-      const filePath = path.join(__dirname, "..", "batterysync-android.apk");
-      res.sendFile(filePath);
-      const stats = fs.statSync(filePath);
-      log(
-        null,
-        "/file/android",
-        "GET",
-        req.rawBodySize,
-        stats.size || 0,
-        user.id
-      );
+    const auth = req.headers.authorization || null;
+    if (auth) {
+      const user = await Users.findOne({ where: { password: auth } });
+      if (user) {
+        const filePath = path.join(__dirname, "..", "batterysync-android.apk");
+        res.sendFile(filePath);
+        const stats = fs.statSync(filePath);
+        log(
+          null,
+          "/file/android",
+          "GET",
+          req.rawBodySize,
+          stats.size || 0,
+          user.id
+        );
+      } else {
+        const filePath = path.join(__dirname, "..", "batterysync-android.apk");
+        res.sendFile(filePath);
+        const stats = fs.statSync(filePath);
+        log(null, "/file/android", "GET", req.rawBodySize, stats.size || 0);
+      }
     } else {
       const filePath = path.join(__dirname, "..", "batterysync-android.apk");
       res.sendFile(filePath);
@@ -40,20 +47,28 @@ router.get("/android", async (req, res) => {
 
 router.get("/macos", async (req, res) => {
   try {
-    const auth = req.headers.authorization;
-    const user = await Users.findOne({ where: { password: auth } });
-    if (user) {
-      const filePath = path.join(__dirname, "..", "batterysync-macos.dmg");
-      res.sendFile(filePath);
-      const stats = fs.statSync(filePath);
-      log(
-        null,
-        "/file/macos",
-        "GET",
-        req.rawBodySize,
-        stats.size || 0,
-        user.id
-      );
+    const auth = req.headers.authorization || null;
+
+    if (auth) {
+      const user = await Users.findOne({ where: { password: auth } });
+      if (user) {
+        const filePath = path.join(__dirname, "..", "batterysync-macos.dmg");
+        res.sendFile(filePath);
+        const stats = fs.statSync(filePath);
+        log(
+          null,
+          "/file/macos",
+          "GET",
+          req.rawBodySize,
+          stats.size || 0,
+          user.id
+        );
+      } else {
+        const filePath = path.join(__dirname, "..", "batterysync-macos.dmg");
+        res.sendFile(filePath);
+        const stats = fs.statSync(filePath);
+        log(null, "/file/macos", "GET", req.rawBodySize, stats.size || 0);
+      }
     } else {
       const filePath = path.join(__dirname, "..", "batterysync-macos.dmg");
       res.sendFile(filePath);
