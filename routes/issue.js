41fd267bc521fac1b9f36f6375cf13f0555a718e
jannikklein47 <jannikklein0302@outlook.de>
2025-12-18 15:12:17 +0100

    removed: debug logs

--- routes/issue.js
+++ routes/issue.js
@@ -151,9 +151,6 @@ router.get("/", async (req, res) => {
 
       // Check if the search includes an @
       if (search.includes("@")) {
-        for (const issue of issues) {
-          console.log(issue);
-        }
         issues = issues.filter(
           (issue) =>
             (issue["user.email"] || "").toLowerCase() ===
