6741db733cd6e94fd95d00f2c9596ee7e83a930c
jannikklein47 <jannikklein0302@outlook.de>
2026-01-29 08:02:56 +0100

    added: logger structure

--- utils/morganMiddleware.js
+++ utils/morganMiddleware.js
@@ -3,6 +3,9 @@
 const morgan = require("morgan");
 const Logger = require("./logger");
 
+"".padEnd(8, " ");
+"".substring(0, 8);
+
 var stream = {
   write: function (message) {
     return Logger.http(message.substring(0, message.lastIndexOf("\n")));
@@ -11,12 +14,14 @@ var stream = {
 var morganMiddleware = morgan(
   function (tokens, req, res) {
     return [
-      tokens.method(req, res),
-      tokens.url(req, res),
+      tokens.method(req, res)?.padEnd(8, " ").substring(0, 8),
       tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
+      tokens.url(req, res)?.padEnd(70, " ").substring(0, 70),
+      (tokens.res(req, res, "content-length") || "0")
+        .padEnd(11, " ")
+        .substring(0, 11),
       "-",
-      tokens["response-time"](req, res),
+      tokens["response-time"](req, res)?.padEnd(8, " ").substring(0, 8),
       "ms",
       req.user ? ` - User (${req.user.id}): ${req.user.email}` : "",
     ].join(" ");
