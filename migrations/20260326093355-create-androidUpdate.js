169ba6a7659ce899a4ba22b8ada2564095804c03
jannikklein47 <jannikklein0302@outlook.de>
2026-03-26 11:46:25 +0100

    changed: release note datatype is text

--- migrations/20260326093355-create-androidUpdate.js
+++ migrations/20260326093355-create-androidUpdate.js
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Updates");
+    await queryInterface.dropTable("AndroidUpdates");
   },
 };
