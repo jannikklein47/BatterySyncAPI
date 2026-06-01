e562892f6f85327b12df3bb91e881f4f3a2e28bd
jannikklein47 <jannikklein0302@outlook.de>
2026-06-01 11:35:46 +0200

    added: battery logs service to get device

--- routes/v1.0/device/getDevice.js
+++ routes/v1.0/device/getDevice.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const DeviceService = require("../../../services/device");
+const BatteryLogsService = require("../../../services/batteryLogs");
 const ValidationRules = require("./validations");
 const APIError = require("../../../utils/error");
 
